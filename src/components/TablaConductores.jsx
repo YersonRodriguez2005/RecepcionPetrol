35a5880c93c5993f36ae7ff47ac45701fedc9f16
Yerson Fabian Garzon Rodriguez <rodriguezyerson2005@gmail.com>
2025-12-30 16:39:49 -0500

    Update TablaConductores.jsx

--- src/components/TablaConductores.jsx
+++ src/components/TablaConductores.jsx
@@ -191,7 +191,6 @@ const ConductoresComponent = () => {
         { cedula: "11111111", conductor: "CARLOS GUTIERREZ", operacion: "TRACTO EXPRESS" },
         { cedula: "72048632", conductor: "MARTINEZ OÑORO LUIS ANIBAL", operacion: "SERVICIOS VARIOS" },
         { cedula: "11111111", conductor: "MARCO ORREGO", operacion: "TRACTO EXPRESS" },
-        { cedula: "11111111", conductor: "DANIEL B", operacion: "TRACTO EXPRESS" }
     ]);
 
     const [search, setSearch] = useState({
